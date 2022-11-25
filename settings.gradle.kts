e344ddbb1dca6b25eaf39729d2e0685e78f5b6f9
Ivan Nikitin <vantoozz@gmail.com>
2022-11-25 15:24:44 -0500

    + github actions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,13 @@
+plugins {
+    id("com.gradle.enterprise") version ("3.11.4")
+}
+
 rootProject.name = "maze"
 include("app")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
